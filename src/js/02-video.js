064b16f2261b0463f491bfd58b72a2e8c42e3557
Iryna-Somyuk <Iryna.Somuyk@gmail.com>
2022-11-11 17:49:54 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+//console.log(Player);
 
+const player = new Player('vimeo-player');
+
+player.on('timeupdate', throttle(PlayOnPlayer, 1000));
+
+function PlayOnPlayer(data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+}
+
+player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,5 @@
+import throttle from 'lodash.throttle';
+
+const key = "feedback-form-state";
+const form = document.querySelector(".feedback-form");
 
